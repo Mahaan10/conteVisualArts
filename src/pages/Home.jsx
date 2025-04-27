fdb452ef586862b91b488d74913a98da9c091263
mahlaghaTavakoli <mahlagha.2211@gmail.com>
2025-04-28 02:49:48 +0330

    modify header section...

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,7 +2,7 @@ import Header from "../ui/Header";
 
 function Home() {
   return (
-    <div className="max-w-screen">
+    <div className="max-w-screen text-neutral-200 overflow-x-hidden">
       <Header />
     </div>
   );
--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -2,20 +2,31 @@ import { Link } from "react-router-dom";
 import CustomNavlink from "./CustomNavlink";
 import { BsSun } from "react-icons/bs";
 import { HiOutlineShoppingBag } from "react-icons/hi";
+import { FaRegUser } from "react-icons/fa";
+import { PiMagnifyingGlassBold } from "react-icons/pi";
+import { toPersianNumbers } from "../utils/toPersianNumbers";
 
 function Header() {
+  const statsInfo = [
+    { id: 1, image: "images/degree.png", num: 987, title: "فارغ التحصیل" },
+    { id: 2, image: "images/courses.png", num: 32, title: "دوره آموزشی" },
+    { id: 3, image: "images/artists.png", num: 1584, title: "هنرجو" },
+  ];
+
   return (
-    <div className="flex flex-col bg-[url('hero-section.webp')] min-h-screen bg-center bg-no-repeat bg-cover text-neutral-200">
+    <div className="flex flex-col relative font-iran-sans min-h-screen">
+      {/* Background Image */}
+      <div className="bg-[url('images/bg-4.jpg')] inset-0 blur-xs absolute bg-center bg-no-repeat bg-cover -z-10"></div>
       {/* Header Navbar */}
-      <div className="flex items-center justify-around">
+      <div className="flex items-center justify-between mt-4 w-[90%] mx-auto">
         {/* Right Section */}
         <ul className="flex items-center gap-x-6">
           <li>
             <Link to="/">
               <img
-                src="IMG_20250427_165334_993.jpg"
+                src="images/IMG_20250427_165334_993-removebg-preview 2.png"
                 alt=""
-                className="w-20 h-20"
+                className="h-16 w-10"
               />
             </Link>
           </li>
@@ -28,16 +39,50 @@ function Header() {
         </ul>
         {/* Left Section */}
         <div className="flex items-center gap-x-6">
-          <button>
+          <button className="cursor-pointer">
             <BsSun className="w-6 h-6" />
           </button>
-          <button>
+          <button className="cursor-pointer">
             <HiOutlineShoppingBag className="w-6 h-6" />
           </button>
-          <Link to="" className="">
-            ورود|عضویت
+          <Link to="" className="btn">
+            <FaRegUser className="w-5 h-5" />
+            <span>ورود|عضویت</span>
           </Link>
         </div>
+      </div>
+      {/* Header Chants */}
+      <div className="container">
+        <div className="text-center mt-8 sm:mt-14">
+          <h1 className="font-black text-4xl">کُنته، جایی برای تجلی هنر</h1>
+          <h1 className="text-xl mt-8 sm:mt-14">
+            با آموزشگاه هنر های تجسمی کُنته، مسیر هنر رو تجربه کن
+          </h1>
+          <div className="mt-8 sm:mt-14 w-[85%] sm:w-[70%] mx-auto relative">
+            <input
+              type="text"
+              className="textField_input"
+              placeholder="جستجو در بین دوره ها ..."
+            />
+            <button className="cursor-pointer absolute top-3.5 p-3 dark:text-neutral-200 left-4 bg-twilight rounded-full">
+              <PiMagnifyingGlassBold className="w-6 h-6" />
+            </button>
+          </div>
+        </div>
+        {/* Stats Section */}
+        <ul className="flex items-center justify-between mt-8 sm:mt-10 w-[60%] mx-auto">
+          {statsInfo.map((stat) => (
+            <li key={stat.id} className="space-y-3 text-center">
+              <div className="">
+                <img src={stat.image} alt="" className="w-24 h-24" />
+              </div>
+              <p className="text-3xl font-black">
+                {toPersianNumbers(stat.num)}
+              </p>
+              <p>{stat.title}</p>
+            </li>
+          ))}
+        </ul>
       </div>
     </div>
   );
