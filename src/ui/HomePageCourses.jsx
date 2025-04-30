e35b80de1a1a4c260a94011fbd2f62d2d61696cb
mahlaghaTavakoli <mahlagha.2211@gmail.com>
2025-04-30 11:33:24 +0330

    modify artist works section...

--- src/ui/HomePageCourses.jsx
+++ src/ui/HomePageCourses.jsx
@@ -1,8 +1,6 @@
 import { Link } from "react-router-dom";
 import { FiArrowUpLeft } from "react-icons/fi";
-import { FaRegUser } from "react-icons/fa";
-import { IoPeopleOutline } from "react-icons/io5";
-import CourseCard from "./courseCard";
+import CourseCard from "./CourseCard";
 
 function HomePageCourses() {
   const lastCourses = [
@@ -49,7 +47,28 @@ function HomePageCourses() {
       <div className="container">
         <div className="mt-10 relative">
           <h1 className="opacity-70 text-xl">آخرین دوره های ما</h1>
-          <div className="sectionTitle"></div>
+          <div className="sectionTitle w-28"></div>
+        </div>
+        <div className="flex items-center justify-between mt-3">
+          <h1 className="text-3xl  font-black">سکوی پرتاب به سمت موفقیت</h1>
+          <Link
+            to=""
+            className="flex items-center gap-x-2 hover:text-sand transition-colors duration-300 font-extrabold opacity-75"
+          >
+            <span className="text-lg">همه دوره ها</span>
+            <FiArrowUpLeft className="w-5 h-5" />
+          </Link>
+        </div>
+        {/* Last Courses */}
+        <div className="grid grid-row-2 sm:grid-cols-2 lg:grid-cols3 xl:grid-cols-4 gap-6 sm:gap-7 mt-10">
+          {lastCourses.map((course) => (
+            <CourseCard key={course.id} course={course} />
+          ))}
+        </div>
+        {/* Artist Work */}
+        <div className="mt-20 relative">
+          <h1 className="opacity-70 text-xl">آثار هنرجویان</h1>
+          <div className="sectionTitle w-22"></div>
         </div>
         <div className="flex items-center justify-between mt-3">
           <h1 className="text-3xl  font-black">سکوی پرتاب به سمت موفقیت</h1>
