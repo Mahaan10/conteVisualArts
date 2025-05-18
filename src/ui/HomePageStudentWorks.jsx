f8e8a24cd839634236ff8626b952d7cf1df7469a
Mahan <reza.mahan10@gmail.com>
2025-05-18 20:02:22 +0330

    modify HomePageStudentWorks section...

--- src/ui/HomePageStudentWorks.jsx
+++ src/ui/HomePageStudentWorks.jsx
@@ -1,5 +1,40 @@
+import { Link } from "react-router-dom";
+import { useToast } from "../context/useToastContext";
+import useStudentWorks from "../hooks/useStudentWorks";
+import Loading from "./Loading";
+
 function HomePageStudentWorks() {
-  return <div>HomePageStudentWorks</div>;
+  const { studentWorks, error, isError, isLoading } = useStudentWorks();
+  const { showToast } = useToast();
+
+  if (isLoading) return <Loading />;
+  if (isError)
+    return showToast(
+      "error",
+      error?.response?.data?.message || "بارگذاری با خطا مواجه شد"
+    );
+  return (
+    <>
+      {studentWorks.map((studentWork) => (
+        <Link
+          key={studentWork._id}
+          to={studentWork.title}
+          className="flex flex-col gap-y-2 items-center w-full"
+        >
+          <div className="w-40 h-40">
+            <img
+              src={studentWork.image}
+              alt={studentWork.title}
+              className="rounded-lg"
+            />
+          </div>
+          <h1 className="text-sm text-nowrap w-40 text-right">
+            {studentWork.title}
+          </h1>
+        </Link>
+      ))}
+    </>
+  );
 }
 
 export default HomePageStudentWorks;
