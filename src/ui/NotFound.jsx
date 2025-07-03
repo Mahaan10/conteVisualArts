9160ce195f1e35c251851bbbbf59c3a9032ba02c
mahlaghaTavakoli <mahlagha.2211@gmail.com>
2025-07-03 12:03:26 +0330

    modify NotFound pages...

--- src/ui/NotFound.jsx
+++ src/ui/NotFound.jsx
@@ -1,3 +1,4 @@
+import { Button, Spinner } from "flowbite-react";
 import { HiArrowRight } from "react-icons/hi";
 import { useNavigate } from "react-router-dom";
 
@@ -9,11 +10,22 @@ function NotFound() {
       <div className="container xl:max-w-screen-xl">
         <div className="sm:max-w-sm mx-auto flex justify-center items-center">
           <div>
-            <h1 className="mb-8 dark:text-whitesmoke text-center text-2xl font-semibold">
-              عدم نمایش اطلاعات
+            <h1 className="my-4 dark:text-whitesmoke text-center text-sm font-semibold sm:text-xl">
+              مشکلی در نمایش اطلاعات به وجود آمده است
             </h1>
+            <p className="mb-4 dark:text-whitesmoke text-center text-xs font-semibold sm:text-lg flex items-center gap-x-4">
+              <span> لطفا از اتصال اینترنت اطمینان حاصل نمایید</span>
+              <div className="flex flex-wrap items-center">
+                <Spinner
+                  size="sm"
+                  aria-label="Info spinner example"
+                  className=""
+                  light
+                />
+              </div>
+            </p>
             <button
-              className="flex items-center gap-x-4 text-blue-600 hover:text-blue-800 dark:text-blue-400 dark:hover:text-blue-600 transition-colors mx-auto"
+              className="flex items-center gap-x-4 text-blue-600 hover:text-blue-800 dark:text-blue-400 dark:hover:text-blue-600 transition-colors mx-auto cursor-pointer"
               onClick={() => navigate(-1)}
             >
               <HiArrowRight className="w-6 h-6" />
